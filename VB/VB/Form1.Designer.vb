a0c9aa796037f8b8a1898581c6d8aa932710c917
Zhiyuan904 <76759169+Zhiyuan0904@users.noreply.github.com>
2021-09-20 22:59:33 +0800

    yayyy can

--- VB/VB/Form1.Designer.vb
+++ VB/VB/Form1.Designer.vb
@@ -25,46 +25,57 @@ Partial Class Form1
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(597, 332)
-        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Location = New System.Drawing.Point(281, 162)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(60, 41)
+        Me.Label1.Size = New System.Drawing.Size(32, 20)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Hiii"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(91, 70)
-        Me.Label2.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label2.Location = New System.Drawing.Point(43, 34)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(299, 41)
+        Me.Label2.Size = New System.Drawing.Size(150, 20)
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "This is ice cream man"
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(578, 533)
+        Me.Button1.Location = New System.Drawing.Point(272, 260)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(1, 1, 1, 1)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(188, 58)
+        Me.Button1.Size = New System.Drawing.Size(88, 28)
         Me.Button1.TabIndex = 2
         Me.Button1.Text = "Hehe"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(266, 82)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(94, 29)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Hellooooo"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(17.0!, 41.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1360, 738)
+        Me.ClientSize = New System.Drawing.Size(640, 360)
+        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
-        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
@@ -75,4 +86,5 @@ Partial Class Form1
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
 End Class
